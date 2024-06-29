2045e5e573753f9e567a27b41dedcd0f434e0159
ONUR AKKOYUN <akkoyunonurcan@gmail.com>
2024-06-29 18:32:16 +0300

    Get user by email added

--- backend/src/main/java/com/tobeto/hotel_reservation_pair_6/services/abstracts/UserService.java
+++ backend/src/main/java/com/tobeto/hotel_reservation_pair_6/services/abstracts/UserService.java
@@ -1,5 +1,7 @@
 package com.tobeto.hotel_reservation_pair_6.services.abstracts;
 
-public interface UserService {
+import com.tobeto.hotel_reservation_pair_6.services.dtos.userDtos.responses.GetUserResponse;
 
+public interface UserService {
+    GetUserResponse getUserByEmail(String email);
 }
