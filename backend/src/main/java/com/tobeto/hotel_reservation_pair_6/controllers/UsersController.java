ec9dbbcb2af53c3346f2fdc8f5b1eea2cbe6f234
ONUR AKKOYUN <akkoyunonurcan@gmail.com>
2024-07-02 02:03:32 +0300

    cors config added

--- backend/src/main/java/com/tobeto/hotel_reservation_pair_6/controllers/UsersController.java
+++ backend/src/main/java/com/tobeto/hotel_reservation_pair_6/controllers/UsersController.java
@@ -4,14 +4,12 @@ import com.tobeto.hotel_reservation_pair_6.services.abstracts.UserService;
 import com.tobeto.hotel_reservation_pair_6.services.dtos.userDtos.responses.GetUserResponse;
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "*", maxAge = 3600)
 @RequiredArgsConstructor
 @RequestMapping("/api/users")
 public class UsersController {
